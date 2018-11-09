e96d0509bc62e7176e2157bbf71217fe4066703f
TheCheatCode <lecheatcode@hotmail.com>
2018-11-09 13:11:33 -0500

    Finished another ReadFile test with content

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class WordSearch {
     public String ReadFile(String fileName) {
-        return "";
+        String result;
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            result = reader.readLine();
+
+            reader.close();
+
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        if (result == null) {
+            result = "";
+        }
+
+        return result;
     }
 }
--- src/test/java/WordSearchTest.java
+++ src/test/java/WordSearchTest.java
@@ -1,12 +1,30 @@
 import org.junit.Test;
 
+import java.io.File;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class WordSearchTest {
+    // Converting resource files into absolute path for test relevance
+    ClassLoader classLoader = getClass().getClassLoader();
+
     @Test
     public void ReadFile_WhenEmptyFile_ReturnsEmptyString() {
         WordSearch wordSearch = new WordSearch();
-        String result = wordSearch.ReadFile("EmptyFile.txt");
+        File file = new File(classLoader.getResource("EmptyFile.txt").getFile());
+
+        String result = wordSearch.ReadFile(file.getAbsolutePath());
+
         assertEquals("", result);
+    }
+
+    @Test
+    public void ReadFile_WhenOneLineFile_ReturnsContent() {
+        WordSearch wordSearch = new WordSearch();
+        File file = new File(classLoader.getResource("OneLineFile.txt").getFile());
+
+        String result = wordSearch.ReadFile(file.getAbsolutePath());
+
+        assertEquals("One line example.", result);
     }
 }
