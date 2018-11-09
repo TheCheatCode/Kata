2383df86d97595e960dd707fd7e95ca7499f5cdf
TheCheatCode <lecheatcode@hotmail.com>
2018-11-09 16:40:17 -0500

    Cleaned up WordSearchTest

--- src/test/java/WordSearchTest.java
+++ src/test/java/WordSearchTest.java
@@ -5,12 +5,12 @@ import java.io.File;
 import static junit.framework.TestCase.assertEquals;
 
 public class WordSearchTest {
+    WordSearch wordSearch = new WordSearch();
     // Converting resource files into absolute path for test relevance
     ClassLoader classLoader = getClass().getClassLoader();
 
     @Test
     public void ReadFile_WhenEmptyFile_ReturnsEmptyString() {
-        WordSearch wordSearch = new WordSearch();
         File file = new File(classLoader.getResource("EmptyFile.txt").getFile());
 
         String result = wordSearch.ReadFile(file.getAbsolutePath());
@@ -20,7 +20,6 @@ public class WordSearchTest {
 
     @Test
     public void ReadFile_WhenOneLineFile_ReturnsContent() {
-        WordSearch wordSearch = new WordSearch();
         File file = new File(classLoader.getResource("OneLineFile.txt").getFile());
 
         String result = wordSearch.ReadFile(file.getAbsolutePath());
