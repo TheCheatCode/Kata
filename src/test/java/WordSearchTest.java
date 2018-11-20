2ce17f779112a74299dd58a107402b19ab83fb71
TheCheatCode <lecheatcode@hotmail.com>
2018-11-20 14:19:06 -0500

    Cleaned up test cases

--- src/test/java/WordSearchTest.java
+++ src/test/java/WordSearchTest.java
@@ -8,6 +8,23 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class WordSearchTest {
+    private static final String EXAMPLE_INPUT_ONE = "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA\n" +
+            "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E\n" +
+            "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G\n" +
+            "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G\n" +
+            "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E\n" +
+            "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D\n" +
+            "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F\n" +
+            "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z\n" +
+            "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P\n" +
+            "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S\n" +
+            "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K\n" +
+            "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D\n" +
+            "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K\n" +
+            "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H\n" +
+            "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S\n" +
+            "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B";
+
     WordSearch wordSearch = new WordSearch();
     // Converting resource files into absolute path for test relevance
     ClassLoader classLoader = getClass().getClassLoader();
@@ -93,22 +110,7 @@ public class WordSearchTest {
     public void ReadWords_WhenFullWordSearchInput_ReturnsWordArray() {
         String[] expected = {"BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"};
 
-        String[] result = wordSearch.ReadWords("BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA\n" +
-                "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E\n" +
-                "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G\n" +
-                "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G\n" +
-                "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E\n" +
-                "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D\n" +
-                "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F\n" +
-                "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z\n" +
-                "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P\n" +
-                "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S\n" +
-                "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K\n" +
-                "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D\n" +
-                "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K\n" +
-                "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H\n" +
-                "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S\n" +
-                "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B");
+        String[] result = wordSearch.ReadWords(EXAMPLE_INPUT_ONE);
 
         assertTrue(Arrays.equals(expected, result));
     }
@@ -158,22 +160,7 @@ public class WordSearchTest {
                 {'W','Z','M','I','S','U','K','U','R','B','I','D','U','X','S'},
                 {'K','Y','L','B','Q','Q','P','M','D','F','C','K','E','A','B'}};
 
-        char[][] result = wordSearch.ReadGrid("BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA\n" +
-                "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E\n" +
-                "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G\n" +
-                "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G\n" +
-                "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E\n" +
-                "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D\n" +
-                "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F\n" +
-                "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z\n" +
-                "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P\n" +
-                "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S\n" +
-                "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K\n" +
-                "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D\n" +
-                "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K\n" +
-                "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H\n" +
-                "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S\n" +
-                "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B");
+        char[][] result = wordSearch.ReadGrid(EXAMPLE_INPUT_ONE);
 
         CompareGrids(expected, result);
     }
